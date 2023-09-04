cdbf6db0f6285f81bbcb99a38b9129b616c5064c
preandero <preandero@gmail.com>
2023-09-04 22:40:52 +0900

    feat: Interceptor
    - swagger 예외처리

--- src/main/kotlin/com/bookpiseo/configuration/WebMvcConfig.kt
+++ src/main/kotlin/com/bookpiseo/configuration/WebMvcConfig.kt
@@ -11,6 +11,7 @@ class WebMvcConfig(private val sessionInterceptor: SessionInterceptor) : WebMvcC
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(sessionInterceptor)
                 .addPathPatterns("/**") // 모든 요청에 대해 Interceptor 적용
-                .excludePathPatterns("/api/login", "/api/home/**") // 예외 URL 설정
+                .excludePathPatterns("/api/login", "/api/home/**", "/swagger-ui/**", "/v3/api-docs/**"
+                ) // 예외 URL 설정
     }
 }
